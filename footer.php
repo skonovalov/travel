f73d66266bc7fa2cf92e4b228ec4b7c5fd834063
skonovalov <kanych7@gmail.com>
2016-12-21 16:19:59 +0300

    change place of index file

---
+++ footer.php
@@ -0,0 +1,75 @@
+<?php 
+
+echo '<footer>
+	<div class="footer-bg">
+		<img src="../images/footer-bg.png" alt="footer-bg">
+	</div>
+	<div class="footer-inner container">
+		<div class="row">
+			<div class="menu">
+				<ul class="row">
+					<li><a href="#">home</a></li>
+					<li><a href="#">about us</a></li>
+					<li><a href="#">shore exursions</a></li>
+					<li><a href="#">tours</a></li>
+					<li><a href="#">travel info</a></li>
+					<li><a href="#">f.a.q</a></li>
+					<li><a href="#">roll call</a></li>
+					<li><a href="#">contact us</a></li>
+				</ul>
+			</div>
+			<div class="footer-bottom">
+				<div class="footer-item">
+					<ul>
+						<li>&copy; &laquo; Aventura &raquo;</li>
+						<li>travel Agency in Russia</li>
+						<li>2007 - 2015</li>
+					</ul>
+				</div>
+				<div class="footer-item">
+					<ul>
+						<li><a href="">About us</a></li>
+						<li><a href="">Testimonials</a></li>
+					</ul>
+					<article>shore exursions</article>
+					<ul>
+						<li><a href="">1 day tour</a></li>
+						<li><a href="">2 days tour</a></li>
+						<li><a href="">3 days tour</a></li>
+					</ul>
+				</div>
+				<div class="footer-item">
+					<ul>
+						<li><a href="">Saint-Petersburg tours</a></li>
+						<li><a href="">Moscow tours</a></li>
+						<li><a href="">Moscow and Golden Ring of Russia</a></li>
+						<li><a href="">Tours in Baltic Countries</a></li>
+					</ul>
+				</div>
+				<div class="footer-item">
+					<ul>
+						<li><a href="">Info for travelers</a></li>
+						<li><a href="">Cultural Events</a></li>
+						<li><a href="">St. Petersburg museums</a></li>
+						<li><a href="">Suburbs of Saint-Petersburg</a></li>
+						<li><a href="">Air and rail travelers</a></li>
+						<li><a href="">TOP-10 places to visit</a></li>
+					</ul>
+				</div>
+				<div class="footer-item">
+					<ul>
+						<li><a href="#"><span class="icon-tripadvisor"></a></span></li>
+						<li><a href="#"><span class="icon-google"></a></span></li>
+						<li><a href="#"><span class="icon-facebook"></a></span></li>
+					</ul>
+				</div>
+			</div>
+		</div>	
+	</div>
+</footer>	
+<script src="http://yastatic.net/jquery/1.9.0/jquery.min.js"></script>
+<script src="../js/script.js"></script>
+</body>
+</html>';
+
+?>
